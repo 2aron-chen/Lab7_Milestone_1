4c4407bac977c37cda4b061d7e6f9287c93cf4d8
kuanf <kuanfuchen88@gmail.com>
2021-11-10 15:47:44 -0600

    Add App.java.

--- app/src/main/java/com/example/lab7_milestone_1/MainActivity.java
+++ app/src/main/java/com/example/lab7_milestone_1/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.lab7_milestone_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
     }
 }
